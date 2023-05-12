b2ac9b8272585245731d88ac4939b0a99d201140
vikasr54389 <vikasr54389@gmail.com>
2023-05-12 13:01:20 +0530

    this is my api project using flask code

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask,request ,render_template , jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+
+@app.route('/math',methods=['POST'])
+def math_ops():
+    if(request.method == 'POST'):
+        ops = request.form['operation']
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        if ops == 'add':
+            r = num1+num2
+            result = "The sum of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if ops == 'subtract':
+            r = num1-num2
+            result = "The subtract of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if ops == 'multiply':
+            r = num1*num2
+            result = "The multiply of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if ops == 'divide':
+            r = num1/num2
+            result = "The divide of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+            
+        return render_template('results.html' , result = result)
+
+
+
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+   app.run(host="0.0.0.0")
